5fb8521cc88a43b19159c628df018ce49ab2c358
Mayra Amezcua <mamezcua.mma@gmail.com>
2020-09-02 16:17:25 -0700

    JS minor edits

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const myGreeting = document.getElementById('greeting');
 const myInput = document.getElementById('greeting-input');
 const myButton = document.getElementById('greeting-button');
 
+// DOM - document object model --> How we get stuff from HTML to JS
 // This is how we tell the button  to listen for clicks.
 myButton.addEventListener('click', ()=>{
     //console.log(Date());
